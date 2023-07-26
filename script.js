c0b7c5d0922c820401144a20556b50009cad4e82
Paulo Abelha Ferreira <pauloabelha@gmail.com>
2023-07-25 22:43:42 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', function() {
       if (xhr.readyState === XMLHttpRequest.DONE) {
         if (xhr.status === 200) {
           const essayContent = xhr.responseText;
-          document.getElementById('essayContent').innerHTML = Marked(essayContent);
+          document.getElementById('essayContent').innerHTML = marked.parse(essayContent);
         } else {
           document.getElementById('essayContent').innerHTML = 'Error loading the essay.';
         }
