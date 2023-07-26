0859d0099d6058962b0a673c9cda74421bb4267d
Paulo Abelha Ferreira <pauloabelha@gmail.com>
2023-07-25 22:20:51 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
 document.addEventListener('DOMContentLoaded', function() {
-  // Your code here, including the loadEssay function
+  // Define the loadEssay function
   function loadEssay(filename) {
-  const xhr = new XMLHttpRequest();
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState === XMLHttpRequest.DONE) {
-      if (xhr.status === 200) {
-        const essayContent = xhr.responseText;
-        document.getElementById('essayContent').innerHTML = window.marked(essayContent);
-      } else {
-        document.getElementById('essayContent').innerHTML = 'Error loading the essay.';
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState === XMLHttpRequest.DONE) {
+        if (xhr.status === 200) {
+          const essayContent = xhr.responseText;
+          document.getElementById('essayContent').innerHTML = window.marked(essayContent);
+        } else {
+          document.getElementById('essayContent').innerHTML = 'Error loading the essay.';
+        }
       }
-    }
-  };
-  xhr.open('GET', 'essays/' + filename, true);
-  xhr.send();
-}
-});
+    };
+    xhr.open('GET', 'essays/' + filename, true);
+    xhr.send();
+  }
 
+  // The rest of your code (if any) goes here
+  // ...
+});
 
